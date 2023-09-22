9cb12a8c0a4509fb57a88a5b21c0152528edc562
marina pessoa <marinacp.pessoa@gmail.com>
2023-09-22 18:40:30 -0300

    tests for link to register magical land

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "Welcome", type: :feature do
   scenario "Shows welcome message" do
-    visit('/') # Root
+    visit(root_path)
     expect(page).to have_content("Welcome")
+  end
+
+  scenario "Verify link for register of magical land" do
+    visit(root_path)
+    # expect(page).to have_selector("ul li a")
+    # expect(page).to have_link("Register magical land")
+    # Refactor
+    expect(find("ul li")).to have_link("Register magical land")
   end
 end
